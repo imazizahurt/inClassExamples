c4c9c3206addff0fc2a742f3ba2e2b0c7ae7c2d5
Aziza Hurt <imazizahurt@gmail.com>
2019-08-29 14:15:39 -0500

    number game with extras

--- inClassEx/Program.cs
+++ inClassEx/Program.cs
@@ -1,43 +1,111 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 
-namespace inClassEx
+namespace inClassExamples
 {
     class Program
     {
-        static void Main(string[] args)
+        static int GetIntegerFromUser(string question)
         {
-            User user = new User();
-            Console.WriteLine("Hey, Can I ask you a little about yourself?");
-
-            user.FirstName = user.AnswerQuestion("What's your first name?");
-            user.LastName = user.AnswerQuestion("What's your last name?");
-            user.FavAnimal = user.AnswerQuestion("What's your favorite animal?");
-            user.FavColor = user.AnswerQuestion("How about your favorite color...");
-            user.FavNumber = user.AnswerQuestionWithInteger("What's your lucky number?");
-
-            Console.WriteLine($"So, you are {user.FirstName} {user.LastName}, and your favorite color is {user.FavColor}!");
-            Console.WriteLine($"Your spirit animal is {user.FavAnimal}!");
-
-            Console.Write($"And your lucky number is");
-
-            if(user.FavNumber > 10)
+            int integerFromUser;
+            bool success;
+            do
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"{user.FavNumber}");
-                Console.ForegroundColor = ConsoleColor.Black;
-            }
+                Console.WriteLine(question);
+                string userResponse = Console.ReadLine();
+                success = int.TryParse(userResponse, out integerFromUser);
+            } while (success == false);
 
-            else if(user.FavNumber < 10)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"{user.FavNumber}");
-                Console.ForegroundColor = ConsoleColor.Black;
-            }
-
-
+            return integerFromUser;
         }
 
-        
+        static void GiveUserARandomInsult(string[] availableInsults)
+        {
+            Random random = new Random();
+            int insultIndex = random.Next(0, availableInsults.Length);
+            string insult = availableInsults[insultIndex];
+            Console.WriteLine(insult);
+        }
+
+        static void RespondToIncorrectGuess(string[] insultsToBeUsed, string highLowHint)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(highLowHint);
+            GiveUserARandomInsult(insultsToBeUsed);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's play a guessing game!");
+
+            int max = GetIntegerFromUser("What max range would you like?");
+
+            Random rnd = new Random();
+            int secretNumber = rnd.Next(1, max + 1);
+
+            string[] insults = {
+                "You ugly...You your daddy son!!",
+                "You are quite the guesser...NOT!!!",
+                "Maybe you should try a different game..Perhaps dodge the cars in traffic?",
+                "Try " + (secretNumber + 1) + ". No, really, you should try " + (secretNumber +1) + "."
+            };
+ 
+            int score = 0;
+            int guess;
+            bool shouldLaunchVideo = true;
+
+            do
+            {
+                Console.ResetColor();
+
+                Console.WriteLine("Your current score is " + score);
+                guess = GetIntegerFromUser("Please guess a number between 1-" + max + ":");
+
+
+                if (guess > max || guess < 1)
+                {
+                    Console.WriteLine("So, apparently guessing isn't your thing");
+
+                    if(shouldLaunchVideo == true)
+                    {
+                        shouldLaunchVideo = false;
+                        Console.WriteLine("Heres a tutorial that's about your competence level:");
+
+                        System.Threading.Thread.Sleep(4000);
+
+                        // mac
+                        Process.Start("open", "https://www.youtube.com/watch?v=9QDZUrvNI-0");
+
+                        //windows
+                        //Process.Start(new ProcessStartInfo("cmd", $"/c start https://www.youtube.com/watch?v=9QDZUrvNI-0"));
+                    }
+
+                    score += 10;
+
+                }
+
+                else 
+                {
+                    if(guess > secretNumber)
+                    {
+                        RespondToIncorrectGuess(insults, "You were too high, loser!");
+                        score += 1;
+                    }
+                    else if(guess < secretNumber)
+                    {
+                        RespondToIncorrectGuess(insults, "You were too low, loser!");
+                    }
+                }
+
+            } while (guess != secretNumber);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Finally...you got it!!!");
+            Console.WriteLine("Your final score was " + score);
+            Console.WriteLine("Next time, May the odds be ever in your favor...Or whatever!!");
+        }
+
+
+
     }
 }
