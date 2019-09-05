f9f1b1b29bab0b40937d42c52621761fa04ae3ca
Aziza Hurt <imazizahurt@gmail.com>
2019-09-05 10:34:22 -0500

    Examples of Extension methods

--- inClassEx/Program.cs
+++ inClassEx/Program.cs
@@ -1,111 +1,47 @@
 ﻿using System;
-using System.Diagnostics;
+using System.Linq;
 
-namespace inClassExamples
+namespace inClassEx
 {
     class Program
     {
-        static int GetIntegerFromUser(string question)
-        {
-            int integerFromUser;
-            bool success;
-            do
-            {
-                Console.WriteLine(question);
-                string userResponse = Console.ReadLine();
-                success = int.TryParse(userResponse, out integerFromUser);
-            } while (success == false);
 
-            return integerFromUser;
-        }
-
-        static void GiveUserARandomInsult(string[] availableInsults)
-        {
-            Random random = new Random();
-            int insultIndex = random.Next(0, availableInsults.Length);
-            string insult = availableInsults[insultIndex];
-            Console.WriteLine(insult);
-        }
-
-        static void RespondToIncorrectGuess(string[] insultsToBeUsed, string highLowHint)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(highLowHint);
-            GiveUserARandomInsult(insultsToBeUsed);
-        }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Let's play a guessing game!");
+            int[] numbers = { 3, 5, 7, 3, 7, 2, 1 };
+            int total = numbers.Sum();
 
-            int max = GetIntegerFromUser("What max range would you like?");
+            int x = 10;
 
-            Random rnd = new Random();
-            int secretNumber = rnd.Next(1, max + 1);
+            int y = x.Multiply(99);
 
-            string[] insults = {
-                "You ugly...You your daddy son!!",
-                "You are quite the guesser...NOT!!!",
-                "Maybe you should try a different game..Perhaps dodge the cars in traffic?",
-                "Try " + (secretNumber + 1) + ". No, really, you should try " + (secretNumber +1) + "."
-            };
- 
-            int score = 0;
-            int guess;
-            bool shouldLaunchVideo = true;
+            string lastLetter = "Hello".GetLastCharacter();
 
-            do
-            {
-                Console.ResetColor();
+            Console.WriteLine(y);
 
-                Console.WriteLine("Your current score is " + score);
-                guess = GetIntegerFromUser("Please guess a number between 1-" + max + ":");
-
-
-                if (guess > max || guess < 1)
-                {
-                    Console.WriteLine("So, apparently guessing isn't your thing");
-
-                    if(shouldLaunchVideo == true)
-                    {
-                        shouldLaunchVideo = false;
-                        Console.WriteLine("Heres a tutorial that's about your competence level:");
-
-                        System.Threading.Thread.Sleep(4000);
-
-                        // mac
-                        Process.Start("open", "https://www.youtube.com/watch?v=9QDZUrvNI-0");
-
-                        //windows
-                        //Process.Start(new ProcessStartInfo("cmd", $"/c start https://www.youtube.com/watch?v=9QDZUrvNI-0"));
-                    }
-
-                    score += 10;
-
-                }
-
-                else 
-                {
-                    if(guess > secretNumber)
-                    {
-                        RespondToIncorrectGuess(insults, "You were too high, loser!");
-                        score += 1;
-                    }
-                    else if(guess < secretNumber)
-                    {
-                        RespondToIncorrectGuess(insults, "You were too low, loser!");
-                    }
-                }
-
-            } while (guess != secretNumber);
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Finally...you got it!!!");
-            Console.WriteLine("Your final score was " + score);
-            Console.WriteLine("Next time, May the odds be ever in your favor...Or whatever!!");
+            Console.WriteLine(lastLetter);
         }
 
 
-
     }
+
+    static class IntegerExtensions
+    {
+        public static int Multiply(this int number, int multiplyBy)
+        {
+            return number * multiplyBy;
+        }
+
+        public static string GetLastCharacter(this string str)
+        {
+            return str[str.Length - 1].ToString();
+        }
+    }
+
+
+
+
+
+    
 }
