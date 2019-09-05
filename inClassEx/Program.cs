b94939c959fb30057001004781265b36d43fc3d1
Aziza Hurt <imazizahurt@gmail.com>
2019-09-05 10:38:43 -0500

    added descriptive comments

--- inClassEx/Program.cs
+++ inClassEx/Program.cs
@@ -10,12 +10,16 @@ namespace inClassEx
         static void Main(string[] args)
         {
             int[] numbers = { 3, 5, 7, 3, 7, 2, 1 };
-            int total = numbers.Sum();
+            int total = numbers.Sum(); //Sum() is an extension method in the System.Linq namespace
+                                      //Sum() is not a method of the int[] class itself
 
             int x = 10;
 
+            //The in class does not have a Multiply method
+            //Multiply is an extension method we wrote below
             int y = x.Multiply(99);
 
+            //GetLastCharacter is an extension method written below
             string lastLetter = "Hello".GetLastCharacter();
 
             Console.WriteLine(y);
@@ -28,20 +32,9 @@ namespace inClassEx
 
     static class IntegerExtensions
     {
-        public static int Multiply(this int number, int multiplyBy)
-        {
-            return number * multiplyBy;
-        }
-
-        public static string GetLastCharacter(this string str)
-        {
-            return str[str.Length - 1].ToString();
-        }
+        public static int Multiply(this int number, int multiplyBy) => number * multiplyBy;
+       
+        public static string GetLastCharacter(this string str) => str[str.Length - 1].ToString(); 
+      
     }
-
-
-
-
-
-    
 }
