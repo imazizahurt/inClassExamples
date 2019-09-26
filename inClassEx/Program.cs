3c4a85d9b43a1bebc9377856b0245d2e5f2901ef
Aziza Hurt <imazizahurt@gmail.com>
2019-09-26 07:55:49 -0500

    recursion learn

--- inClassEx/Program.cs
+++ inClassEx/Program.cs
@@ -7,44 +7,30 @@ namespace Practice_Method
     
         static void Main(string[] args)
         {
-            //DivideByZero Exception
-
-            int num1 = 800;
-            int num2 = 0;
-
-            try
-            {
-                Console.WriteLine(num1/num2);
-            }
-
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            finally
-            {
-                Console.WriteLine("Pick a different divisor!");
-            }
-
-            //OutOfRange Exception
-
-            int[] goodArray = { 1, 5, 7, 8, 9 };
-
-            try
-            {
-                Console.WriteLine(goodArray[5]);
-            }
-
-            catch(Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-
-
-
+    
+            Console.WriteLine(Factorial(5));
+            Console.WriteLine(Factorial(10));
+            
         }
 
+        static int Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            // recursive solution (as opposed to iterative)
+            return n * Factorial(n - 1);
+
+                // e.g.
+                // 5! = 5 * 4!
+                //  4! = 4 * 3!
+                //   3! = 3 * 2!
+                //    2! = 2 * 1!
+                //     1! = 1 * 0!
+                //      0! = 1 (because math)
+        }
     }
 }
+
